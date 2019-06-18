9c60f43ccedc1d5d0274e670390f19af8267f883
danpatrickoneill <dan47573@gmail.com>
2019-06-18 15:20:20 -0400

    Initial commit - get endpoint pulling seed data

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const server = require('./server.js');
+const express = require("express");
+
+const server = require("./server.js");
+
+const db = require("./data/accounts-model");
 
 const PORT = process.env.PORT || 4000;
+
+server.get("/accounts", (req, res) => {
+  db.find()
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error retrieving accounts data." });
+    });
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-// your code here
+function logger(req, res, next) {
+  console.log(req.method, req.url, new Date().toISOString());
+
+  next();
+}
+
+server.use(express.json());
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h1>Hello!</h1>`);
+});
 
 module.exports = server;
